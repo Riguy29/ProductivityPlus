b616586e2e8f086d0251328e409eb87ec4e62131
Riguy29 <riguygb@gmail.com>
2022-10-05 22:14:28 -0500

    Scene Update

---
+++ Productivity+/src/application/ProductivityPlusController.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
+
+public class ProductivityPlusController {
+
+    @FXML
+    private MenuItem workPresetLayout;
+
+    @FXML
+    private MenuItem studyPresetLayoutMenu;
+
+    @FXML
+    private MenuItem dailyTaskListCreation;
+
+    @FXML
+    private MenuItem aboutMenuItem;
+    
+    @FXML
+    private Text test;
+    
+    @FXML
+    void onAboutButtonClick(ActionEvent event) {
+    	test.setText("This has been changed");
+    }
+
+}
+
