ca67005e70f96a3c34ba4f773b4cf030f30d3ddd
Riguy29 <riguygb@gmail.com>
2022-10-24 22:13:02 -0500

    More comments and small changes to brain warmup

--- Productivity+/src/Modules/brainWarmupController.java
+++ Productivity+/src/Modules/brainWarmupController.java
@@ -52,26 +52,36 @@ public class brainWarmupController  extends baseModuleInitalizer{
 			@Override
 			public void handle(ActionEvent arg0) {
 				
-				if(submitButton.getText().equals("Close Quiz")){
-					
+				if(submitButton.getText().equals("Next Question")){
+					nextQuestion();
+					submitButton.setText("Submit");
+					answerTextField.setVisible(true); //Show textfield for user to enter answer
 				}
-				else {
+				else { //If the button is in submit mode
 					
 					String userAnswer = answerTextField.getText();
 					userAnswer.toLowerCase();
 					if(userAnswer.equals(answer)) {
 						System.out.print("Correct!");
+						questionLabel.setText("Correct");
 						amtCorrect += 1;
 					}
-					if(QuizQuestions.isEmpty() == true) {
+					else {
+						System.out.print("Correct!");
+						questionLabel.setText("Incorrect");
+					}
+					
+					
+					if(QuizQuestions.isEmpty() == true) { //If the List is empty show end screen with score
 						//TODO: Show a end screen with the users score
 						parentVBox.getChildren().removeAll(answerTextField,submitButton);
 						questionLabel.setText(String.format("You scored %d/%d", amtCorrect,totalQuestions));
 					}
-					else {
-						nextQuestion();
+					else { //Else let user to go to next question
+						submitButton.setText("Next Question");
+						answerTextField.setVisible(false); //Hides answer text field
+						answerTextField.setText(""); //Clears answer box
 					}
-					answerTextField.setText(""); //Clears answer box
 				}
 
 				
@@ -89,20 +99,21 @@ public class brainWarmupController  extends baseModuleInitalizer{
 		
     	//TODO: Add pictures in 
     	//Fill in Quiz Arrays
-    	QuizQuestions.add("What has cities, but no houses; forests, but no trees; and water, but no fish");
-    	QuizAnswer.add("map");
-    	QuizQuestions.add("A bat and a ball cost $1.10 in total. The bat costs $1.00 more than the ball. How many cents does the ball cost?");
+    	QuizQuestions.add("What has cities, but no houses;\n forests, but no trees;\n and water, but no fish");
+    	QuizAnswer.add("a map");
+    	QuizQuestions.add("A bat and a ball cost $1.10 in total.\n The bat costs $1.00 more than the ball.\n How many cents does the ball cost?");
     	QuizAnswer.add("5");
     	QuizQuestions.add("Whats 3 + 3*3 /3");
     	QuizAnswer.add("6");
-    	QuizQuestions.add("A Red House Is Made From Red Bricks. A Blue House Is Made From Blue Bricks. A Yellow House Is Made From Yellow Bricks. What Is A Green House Made From?");
+    	QuizQuestions.add("A Red House Is Made From Red Bricks.\n A Blue House Is Made From Blue Bricks.\n A Yellow House Is Made From Yellow Bricks.\n What Is A Green House Made From?");
     	QuizAnswer.add("glass");
     	QuizQuestions.add("David’s Father Has Three Sons: Snap, Crackle, And ________?");
     	QuizAnswer.add("david");
-    	QuizQuestions.add("A girl fell off of a 20ft ladder. She wasn't hurt. How?");
-    	QuizAnswer.add("She was on the bottom step");
-    	QuizQuestions.add("There’s a one-story house where everything is yellow. The walls are yellow. The doors are yellow. All the furniture is yellow. The house has yellow beds and yellow couches. What color are the stairs?");
-    	QuizAnswer.add("There are no stairs—it’s a one-story house!");
+    	//Commented these out until we get a more complex detection system -Riley
+//    	QuizQuestions.add("A girl fell off of a 20ft ladder. She wasn't hurt. How?");
+//    	QuizAnswer.add("She was on the bottom step");
+//    	QuizQuestions.add("There’s a one-story house where everything is yellow. The walls are yellow. The doors are yellow. All the furniture is yellow. The house has yellow beds and yellow couches. What color are the stairs?");
+//    	QuizAnswer.add("There are no stairs—it’s a one-story house!");
 	}
     
     private void nextQuestion() {
